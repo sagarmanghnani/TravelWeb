cf32a9668d5a2df3f1dfea204d74f0ce9c9304f9
sagarmanghnani <sagarmanghnani@gmail.com>
2018-09-24 10:32:17 +0530

    port update

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ app.use(function(req, res, next) {
 });
  
 app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+// app.set('port', process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode");
 });
+// app.listen(app.get('port'), function () {
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
